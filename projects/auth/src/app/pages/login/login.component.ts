2dccbbabfdc39126f30074145cc58582e347505b
zhuchentong <2037630@163.com>
2020-10-29 18:23:00 +0800

    update

--- projects/auth/src/app/pages/login/login.component.ts
+++ projects/auth/src/app/pages/login/login.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core'
+import { navigateToUrl } from 'single-spa'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.less']
 })
 export class LoginComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public onLogin() {
+    navigateToUrl('/app01/page1')
   }
-
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,13 +6,44 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations'
 
+import { NzModalModule } from 'ng-zorro-antd/modal'
+import { IconsProviderModule } from '../icons-provider.module'
+import { NzLayoutModule } from 'ng-zorro-antd/layout'
+import { NzMenuModule } from 'ng-zorro-antd/menu'
+import { NzDrawerModule } from 'ng-zorro-antd/drawer'
+import { NzTableModule } from 'ng-zorro-antd/table'
+import { NzSpaceModule } from 'ng-zorro-antd/space'
+import { NzGridModule } from 'ng-zorro-antd/grid'
+import { NzButtonModule } from 'ng-zorro-antd/button'
+import { NzPipesModule } from 'ng-zorro-antd/pipes'
+
+const ANTD_MODULES = [
+  NzModalModule,
+  NzLayoutModule,
+  NzMenuModule,
+  NzDrawerModule,
+  NzTableModule,
+  NzSpaceModule,
+  NzGridModule,
+  NzButtonModule,
+  NzPipesModule,
+  IconsProviderModule
+]
 const COMPONENTS = []
 const DIRECTIVES = []
 const PIPES = []
 const THIRDS = []
 
 @NgModule({
-  imports: [BrowserModule, BrowserAnimationsModule, CommonModule, FormsModule, RouterModule, ReactiveFormsModule],
+  imports: [
+    ...ANTD_MODULES,
+    BrowserModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    ReactiveFormsModule
+  ],
   providers: [],
   entryComponents: [...COMPONENTS],
   declarations: [...COMPONENTS, ...DIRECTIVES, ...PIPES],
@@ -23,6 +54,7 @@ const THIRDS = []
     BrowserAnimationsModule,
     ReactiveFormsModule,
     RouterModule,
+    ...ANTD_MODULES,
     ...COMPONENTS,
     ...DIRECTIVES,
     ...PIPES
