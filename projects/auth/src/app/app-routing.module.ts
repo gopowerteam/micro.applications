ca4a2ff44afc35a823da0c852aaad9b82dc43fc1
zhuchentong <2037630@163.com>
2020-10-29 11:05:46 +0800

    update

--- projects/auth/src/app/app-routing.module.ts
+++ projects/auth/src/app/app-routing.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
-import { Page1Component } from './pages/page1/page1.component'
-import { Page2Component } from './pages/page2/page2.component'
+import { LoginComponent } from './pages/login/login.component'
+import { RegisterComponent } from './pages/register/register.component'
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'page1',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
-    path: 'page1',
-    component: Page1Component
+    path: 'login',
+    component: LoginComponent
   },
   {
-    path: 'page2',
-    component: Page2Component
+    path: 'register',
+    component: RegisterComponent
   }
 ]
 
